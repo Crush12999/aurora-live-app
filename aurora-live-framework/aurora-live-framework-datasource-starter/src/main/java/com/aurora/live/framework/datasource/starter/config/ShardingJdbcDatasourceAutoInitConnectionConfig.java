e7eb354ce574d5001e467f8de8ed86726675fa26
halo <1299975117@qq.com>
2024-03-24 13:53:42 +0800

    feat: ShardingJDBC 数据源自动初始化

---
+++ aurora-live-framework/aurora-live-framework-datasource-starter/src/main/java/com/aurora/live/framework/datasource/starter/config/ShardingJdbcDatasourceAutoInitConnectionConfig.java
@@ -0,0 +1,32 @@
+package com.aurora.live.framework.datasource.starter.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/**
+ * ShardingJDBC 数据源自动初始化
+ * 后续考虑如何将这个配置类做成一个参数控制
+ *
+ * @author halo
+ * @since 2024/3/24 01:52
+ */
+@Configuration
+public class ShardingJdbcDatasourceAutoInitConnectionConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ShardingJdbcDatasourceAutoInitConnectionConfig.class);
+
+    @Bean
+    public ApplicationRunner runner(DataSource dataSource) {
+        return args -> {
+            LOGGER.info("dataSource: {}", dataSource);
+            // 手动触发下连接池的连接创建
+            Connection connection = dataSource.getConnection();
+        };
+    }
+}
