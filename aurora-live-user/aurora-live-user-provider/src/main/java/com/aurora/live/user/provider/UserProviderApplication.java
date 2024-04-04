077d06529e40174c3d0b9ffaa91db3bfcae19cb5
halo <1299975117@qq.com>
2024-04-04 23:59:48 +0800

    test: 测试用户标签设置、取消和是否存在的功能

--- aurora-live-user/aurora-live-user-provider/src/main/java/com/aurora/live/user/provider/UserProviderApplication.java
+++ aurora-live-user/aurora-live-user-provider/src/main/java/com/aurora/live/user/provider/UserProviderApplication.java
@@ -1,6 +1,10 @@
 package com.aurora.live.user.provider;
 
+import com.aurora.live.user.constants.UserTagsEnum;
+import com.aurora.live.user.provider.service.IUserTagService;
+import jakarta.annotation.Resource;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +19,25 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDubbo
 @EnableDiscoveryClient
-public class UserProviderApplication {
+public class UserProviderApplication implements CommandLineRunner {
+
+    @Resource
+    private IUserTagService userTagService;
+
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(UserProviderApplication.class);
         springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Long userId = 1212L;
+        System.out.println("(old) 当前用户是否拥有 is_rich 标签：" + userTagService.containTag(userId, UserTagsEnum.IS_RICH));
+        System.out.println(userTagService.setTag(userId, UserTagsEnum.IS_RICH));
+        System.out.println("(set) 当前用户是否拥有 is_rich 标签：" + userTagService.containTag(userId, UserTagsEnum.IS_RICH));
+        System.out.println(userTagService.cancelTag(userId, UserTagsEnum.IS_RICH));
+        System.out.println("(cancel) 当前用户是否拥有 is_rich 标签：" + userTagService.containTag(userId, UserTagsEnum.IS_RICH));
+
     }
 }
